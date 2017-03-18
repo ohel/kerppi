34843c02b1afb7aa4443b93048e0a2b3847e41bf
ohel <ohel@users.noreply.github.com>
2017-03-18 21:37:28 +0200

    Add rounding. Version bump to 1.0.2.

---
+++ Kerppi/ViewModels/RoundingMultiplyConverter.cs
@@ -0,0 +1,32 @@
+﻿/*
+    Copyright 2015, 2017 Olli Helin / GainIT
+    This file is part of Kerppi, a free software released under the terms of the
+    GNU General Public License v3: http://www.gnu.org/licenses/gpl-3.0.en.html
+*/
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Kerppi.ViewModels
+{
+    class RoundingMultiplyConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return Decimal.Round((System.Convert.ToDecimal(values[0]) * System.Convert.ToDecimal(values[1])), 0, MidpointRounding.AwayFromZero).ToString();
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public object[] ConvertBack(object values, Type[] targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
